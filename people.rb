641eac45d9178c9ef0741b1da0c34c8e3fa6e185
Sabina <sabina_stoicescu@yahoo.com>
2015-04-02 14:30:27 -0400

    Added the final touches to the classes

---
+++ people.rb
@@ -0,0 +1,28 @@
+class Person
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+	def greeting
+		puts "Hi, my name is #{@name}"
+	end
+end
+
+class Student < Person
+	def learn
+		puts "I get it!"	
+	end
+end
+
+class Instructor < Person
+	def teach
+		puts "Everything in Ruby is an Object"	
+	end
+end
+
+
+instructor_one = Instructor.new("Chris")
+instructor_one.greeting
+
