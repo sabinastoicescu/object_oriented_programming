a7480519eba1171db6cadd2af979f9cba7aa8700
Sabina <sabina_stoicescu@yahoo.com>
2015-04-06 10:25:42 -0400

    sales tax minor changes

--- salestax.rb
+++ salestax.rb
@@ -3,27 +3,27 @@ class Goods
 attr_accessor = :item, :cost, :price, :sales_tax, :import_tax 
 
 	def initialize(quantity, item, cost)
-		@quantity
+		
 		@price = price
 		@item=item
 		@cost=cost
+		@sales_tax = sales_tax
+		@import_tax = import_tax
 	end
 
 	def price_calculation
-		@price = @quantity* 1.1*@cost
+		@price = @quantity*(@cost+@sales_tax+@import_tax)
 	end
 	
 	def basic_sales_tax(price)
-		if @item.include?["book","chocolate", "bar","chocolates","headache", "pills"] 
-			then @sales_tax=0
-			else @sales_tax = @price *10%   
+		@sales_tax = 0 if @item.include? ["book","chocolate", "bar","chocolates","headache", "pills"] == true
+		else @sales_tax = @price *10%   
 		end
 	end
 
 	def import_duty(price)
-		if @item.include? "*imported" 
-			then @import_tax = @cost * 5%
-			else @import_tax = 0
+		@import_tax = @cost * 5% if @item.include? "*imported" 
+		else @import_tax = 0
 		end
 	end
 
@@ -55,4 +55,4 @@ shopping_basket3b.price_calculation
 shopping_basket3c =Goods.new(1,"packet of headache pills",9.75)
 shopping_basket3c.price_calculation
 shopping_basket3d = Goods.new(1,"box of imported chocolates",11.25)
-shopping_basket3d.price_calculation
+#shopping_basket3d.price_calculation
